f810c6084753b08691b1dbd28cdcca920bb47abe
unknown <muhamadimuzisamakki@gmail.com>
2024-03-20 07:07:36 -0700

    first commit

---
+++ mainApp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about-us', views.aboutus, name='aboutus'),
+    path('blog', views.blog, name='blog'),
+    path('contact-us', views.contact, name='contact'),
+    path('services', views.services, name='services'),
+]
