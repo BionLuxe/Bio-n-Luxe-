3b3ddfd60ad22faf56d8c0e8826ed05c9ecb7dc6
unknown <muhamadimuzisamakki@gmail.com>
2024-04-23 11:03:59 +0300

    ads

--- bionluxe/urls.py
+++ bionluxe/urls.py
@@ -1,13 +1,12 @@
-
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic.base import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('mainApp.urls')),
     path('summernote/', include('django_summernote.urls')),
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path( "ads.txt",RedirectView.as_view(url=staticfiles_storage.url("ads.txt")),),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
